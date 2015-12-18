6d26233d72be9c8ee60d92c05ce9c08ad1b71595
Kari Bancroft <kariabancroft@gmail.com>
2015-12-18 12:11:55 -0800

    Add another zero for p2

---
+++ four/four-p2.rb
@@ -0,0 +1,23 @@
+require "Digest"
+
+def lowest_number(input)
+  test_value = 1
+  hash_value = Digest::MD5.hexdigest(input + test_value.to_s)
+
+  while !success?(hash_value)
+    test_value += 1
+    hash_value = Digest::MD5.hexdigest(input + test_value.to_s)
+  end
+
+  return test_value
+end
+
+def success?(value)
+  if value.start_with?("000000")
+    return true
+  else
+    return false
+  end
+end
+
+puts lowest_number("bgvyzdsv")
