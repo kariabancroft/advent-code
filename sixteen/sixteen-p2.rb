1057172793c951d8fbfcbd6f04ed204abf3580d9
Kari Bancroft <kariabancroft@gmail.com>
2015-12-27 23:39:49 -0800

    Add part two

---
+++ sixteen/sixteen-p2.rb
@@ -0,0 +1,83 @@
+require 'pry'
+
+def aunt_sues(filename)
+  data = read_file(filename)
+  puts match(data, solution)
+end
+
+def match(data, solution)
+  sues = []
+
+  data.each do |sue|
+    sue_count = 0
+    sue.each do |key, value|
+      if solution.include?(key)
+        case key
+        when "cats", "trees"
+          if solution[key] < value
+            sue_count += 1
+          end
+        when "pomeranians", "goldfish"
+          if solution[key] > value
+            sue_count += 1
+          end
+        else
+          if solution[key] == value
+            sue_count += 1
+          end
+        end
+      end
+    end
+
+    if sue_count >= 3
+      sues << sue[:id]
+    end
+  end
+
+  return sues
+end
+
+def solution
+  return {
+    "children" => 3,
+    "cats" =>  7,
+    "samoyeds" =>  2,
+    "pomeranians" =>  3,
+    "akitas" =>  0,
+    "vizslas" =>  0,
+    "goldfish" =>  5,
+    "trees" =>  3,
+    "cars" =>  2,
+    "perfumes" =>  1
+  }
+end
+
+def convert_input(row)
+  h = {}
+  row = row[4..row.length - 1]
+  data_pieces = row.split(" ")
+  # puts "Data: #{data_pieces}"
+  h[:id] = data_pieces[0].chomp(":").to_i
+
+  (1..data_pieces.length - 1).each do |i|
+    if i % 2 == 1
+      key = data_pieces[i].chomp(":")
+      value = data_pieces[i + 1].chomp(",")
+      h[key] = value.to_i
+    end
+  end
+
+  return h
+end
+
+def read_file(filename)
+  file = []
+  File.open(filename, "r") do |f|
+    f.each_line do |line|
+      file << convert_input(line)
+    end
+  end
+  return file
+end
+
+puts aunt_sues("input.txt")
